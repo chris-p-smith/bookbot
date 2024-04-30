dc6c8c1b67f4bf4c4ba84249f5e29646fbe8a64b
chris-p-smith <chris.p.smith0601@gmail.com>
2024-04-29 21:28:18 -0400

    Completed bot.

--- main.py
+++ main.py
@@ -1,23 +1,52 @@
 def main():
     path = "books/frankenstein.txt"
-    
+    title = get_title(path)
     file_contents = read_books(path)
     word_count = wordcount(file_contents)
-    
-    title = get_title(path)
-    
-    print (f"File {title} has {word_count} words.")
+    chars_dictionary = letter_count(file_contents)
+    chars_list = dict_to_list(chars_dictionary)
+    print (f" --- Begin report --- ")
+    report_print(title,word_count,chars_list)
+    print (" --- End report --- ")
 
-def wordcount(text):
-    words = text.split()
+def get_title(path):
+    path = path.replace('books/', '')
+    return path
+
+def read_books(path):
+    with open(path) as f:
+        return f.read()
+
+def wordcount(file_contents):
+    words = file_contents.split()
     return len(words)
 
-def read_books(book):
-    with open(book) as f:
-        return f.read()
-    
-def get_title(file_path):
-    file_path = file_path.replace('books/', '')
-    return file_path
+def letter_count(file_contents):
+    chars_dictionary = {}
+    for c in file_contents:
+        lowercase = c.lower()
+        if lowercase in chars_dictionary:
+            chars_dictionary[lowercase] += 1
+        else:
+            chars_dictionary[lowercase] = 1
+    return chars_dictionary
+
+def dict_to_list(chars_dictionary):
+    char_list = []
+    for c in chars_dictionary:
+        char_list.append({"char": c, "num": chars_dictionary[c]})    
+    char_list.sort(reverse=True, key=sort_on)
+    return (char_list)
+
+def sort_on(d):
+    return d["num"]
+
+def report_print(title,word_count,chars_dictionary):
+    print (f"Title: {title}")
+    print (f"{word_count} words found in document.")
+
+    for char in chars_dictionary:
+        if char["char"].isalpha():
+            print (f"Letter '{char["char"]}' found {char["num"]} times")
 
 main()
